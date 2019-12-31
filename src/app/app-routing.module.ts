cd5925253422612109bc9f777e358b5ea49d81b1
ruthelsytobar91 <ruthelsytobar91@gmail.com>
2019-12-31 13:38:25 -0500

    primera subida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SesionComponent } from './sesion/sesion.component';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'sesion', component: SesionComponent },
+
+  { path: '', component: InicioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SesionComponent } from './sesion/sesion.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SesionComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
