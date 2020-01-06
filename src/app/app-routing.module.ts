fe4c0b1a6114127ccae3229db9dfc829665bdd72
jose55masepp <josemase55pp@gmail.com>
2020-01-06 16:46:18 -0500

    componente evetos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
 import { SesionComponent } from './sesion/sesion.component';
-import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
+
 
 
 const routes: Routes = [
   { path: 'sesion', component: SesionComponent },
   { path: 'usuario', component: UsuariosComponent },
+  { path: 'eventos' ,component: EventosComponent},
 
   { path: '', component: InicioComponent}
 ];
